ea7acbcef495c58b9633c78a8856618a69ebbce6
wolfiee42 <saifalislam2022@gmail.com>
2025-02-24 19:46:10 +0600

    feat: creating a context for overall form

--- src/contexts/sl-form-context.tsx
+++ src/contexts/sl-form-context.tsx
@@ -1,22 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useContext } from 'react';
-import { Control, FieldValues } from 'react-hook-form';
 
-export type SLFormContext<TFormValues extends FieldValues = any> = {
-	control: Control<TFormValues>;
+import { createContext, useContext } from "react";
+import { Control, FieldValues } from "react-hook-form";
+
+export type FormContext<TFormValues extends FieldValues = any> = {
+  control: Control<TFormValues>;
 };
 
-export const SLFormContext = createContext<null | SLFormContext>(null);
+export const FormContext = createContext<null | FormContext>(null);
 
-/**
- * A hook to use the form context.
- * @returns The form context.
- */
-
-export const useSLFormContext = <TFormValues extends FieldValues = any>() => {
-	const context = useContext(SLFormContext);
-	if (!context) {
-		throw new Error('Form fields must be used within a SmartForm.');
-	}
-	return context as SLFormContext<TFormValues>;
+export const useFormContext = <TFormValues extends FieldValues = any>() => {
+  const context = useContext(FormContext);
+  if (!context) {
+    throw new Error("Form fields must be used within a Form.");
+  }
+  return context as FormContext<TFormValues>;
 };
