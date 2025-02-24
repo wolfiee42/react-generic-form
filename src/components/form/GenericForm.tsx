2cd184ac3e71b5b8611486acd6cdd2796ef6f796
wolfiee42 <saifalislam2022@gmail.com>
2025-02-24 21:38:08 +0600

    feat: creating a wrapper or provider what you want to say for a form, named it generic form

--- src/components/form/GenericForm.tsx
+++ src/components/form/GenericForm.tsx
@@ -1,12 +1,17 @@
-import React, { ReactNode, Ref } from "react";
+import React, { ReactNode, Ref, useImperativeHandle } from "react";
 import {
   Control,
+  DefaultValues,
   FieldValues,
+  Form,
   FormState,
+  Path,
   SubmitHandler,
+  useForm,
   UseFormReturn,
 } from "react-hook-form";
 import { z, ZodType } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export type GenericFormRef<T extends FieldValues> = {
   getValues: () => T;
@@ -24,3 +29,44 @@ export type GenericFormProps<TSchema extends ZodType> = {
   children: ReactNode;
   ref: Ref<GenericFormRef<z.infer<TSchema>>>;
 } & React.ComponentPropsWithoutRef<"form">;
+
+export const GenericForm = <TSchema extends ZodType>({
+  ref,
+  initialValues,
+  schema,
+  onSubmit,
+  children,
+}: GenericFormProps<TSchema>) => {
+  const form = useForm<z.infer<TSchema>>({
+    defaultValues: initialValues as DefaultValues<z.infer<TSchema>>,
+    resolver: zodResolver(schema),
+  });
+
+  useImperativeHandle(ref, () => {
+    type TFormValues = z.infer<TSchema>;
+
+    return {
+      getValues: form.getValues,
+      reset: (values?: Partial<TFormValues>) => {
+        form.reset(values as TFormValues);
+      },
+      setValue: (
+        name: keyof TFormValues,
+        value: TFormValues[keyof TFormValues]
+      ) => {
+        form.setValue(name as Path<TFormValues>, value); //need changes
+      },
+      formState: form.formState,
+      control: form.control,
+      form: form,
+    };
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>{children}</form>;
+    </Form>
+  );
+};
+
+GenericForm.displayName = "GenericForm";
