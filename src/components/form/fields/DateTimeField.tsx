c6aa2b5a012bff0be14f686d3ce9f35ee8347a78
wolfiee42 <saifalislam2022@gmail.com>
2025-02-25 20:07:33 +0600

    feat: creating a generic date time picker

---
+++ src/components/form/fields/DateTimeField.tsx
@@ -0,0 +1,55 @@
+import { FieldValues, Path, useFormContext } from "react-hook-form";
+
+import { DateTimePicker } from "@/components/ui/date-time-picker";
+import { FormField, FormItem, FormLabel, FormMessage } from "../../ui/form";
+
+interface Props<T extends FieldValues> {
+  name: Path<T>;
+  label?: string;
+  required?: boolean;
+  disabled?: boolean;
+}
+
+/**
+ * DateField component
+ *
+ * @param name - The name of the field.
+ * @param label - The label for the field.
+ * @param required - Whether the field is required.
+ * @param disabled - Whether the field is disabled.
+ * @returns The DateField component.
+ */
+
+export const DateTimeField = <T extends FieldValues>({
+  name,
+  label,
+  required = false,
+  disabled = false,
+}: Props<T>) => {
+  const { control } = useFormContext<T>();
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          {label && (
+            <FormLabel htmlFor={name}>
+              <span>{label}</span>
+              {required && <span className="ml-1 text-red-500">*</span>}
+            </FormLabel>
+          )}
+          <DateTimePicker
+            disabled={disabled}
+            value={field.value}
+            onChange={field.onChange}
+          />
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+DateTimeField.displayName = "DateField";
